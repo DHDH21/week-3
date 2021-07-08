85d2965ca3487dabc342f9c98ef63c894b50140b
DHDH <damon.ha6@gmail.com>
2021-07-08 14:36:43 -0400

    updated password length and add copy clipboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,96 @@
-// Starting code
-function getpassword(){
-var key_value = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_-+="
+  
+// The Password generator will provide a password with 8-42  characters based on criteria the user specifies.
 
-var password_length = 12;
-var create_result = "";
-for (var i=0; i<password_length; i++){
-    var generate_ramdon_number = Math.floor(Math.random() * key_value.length);
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-    create_result += key_value.substring(generate_ramdon_number, generate_ramdon_number + 1);
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+// Prompt to confirm how many characters the user would like in their password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 43) {
+      alert("Password length must be between 8-42 characters Try again");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      } 
+
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+
+    // Determine parameters of password 
+    var confirmSpecialCharacter = confirm("Would you like to use Special Character?");
+    var confirmNumericCharacter = confirm("Would you like to use Numeric Character?");    
+    var confirmLowerCase = confirm("Would you like to use Lowercase letters?");
+    var confirmUpperCase = confirm("Would you like to use Uppercase letters?");
+      // Loop if answer is outside the parameters 
+      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+        alert("You must choose at least one parameter");
+        var confirmSpecialCharacter = confirm("Would you like to use Special Character?");
+        var confirmNumericCharacter = confirm("Would you like to use Numeric Character?");    
+        var confirmLowerCase = confirm("Would you like to use Lowercase letters?");
+        var confirmUpperCase = confirm("Would you like to use Uppercase letters?");   
+    } 
+
+      // Assign an action to the password parameters NEED TO FIX THIS
+      var passwordCharacters = []
+      
+    if (confirmSpecialCharacter) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
+    }
+
+    if (confirmNumericCharacter) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+      
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(alphaLower)
+    }
+
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(alphaUpper)
+    }
+
+      console.log(passwordCharacters)
+
+      // Empty string to be filled based on for loop selecting random characters from the array
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
 }
 
-document.getElementById("password").value = create_result;
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
+
+
+
+// write copy to password to clipboard
+
+function myFunction() {
+  var copyToClipboard = document.getElementById("password");
+  copyToClipboard.select();
+  copyToClipboard.setSelectionRange(0,53)
+  document.execCommand("copy");
+  window.alert("Copied the text: " + copyToClipboard.value);
 }
